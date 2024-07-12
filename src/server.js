014284c4b06ca4b491ac8941b07379fee19d172b
Brendan Smyers <bs602422@ohio.edu>
2024-07-12 00:36:28 -0400

    redirected http traffic to https

--- src/server.js
+++ src/server.js
@@ -1,17 +1,30 @@
 require('dotenv').config();
+const express = require('express');
 const https = require('https');
+const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const app = require('./app');
 
+const domain = process.env.NODE_ENV == 'production' ? process.env.PRODUCTION_DOMAIN : `localhost`
+
+// Setup https server
 const key = fs.readFileSync(path.join(__dirname, '../key.pem'));
 const cert = fs.readFileSync(path.join(__dirname, '../cert.pem'));
 const credentials = { key: key, cert: cert };
 
-const server = https.createServer(credentials, app);
+const httpsServer = https.createServer(credentials, app);
 
-const PORT = process.env.PORT || 3000;
+// Setup http server to redirect traffic to https server
+const httpApp = express();
+httpApp.all('*', (req, res) => res.redirect(301, `https://${domain}:${process.env.HTTPS_PORT}${req.originalUrl}`));
 
-server.listen(PORT, () => {
-    console.log(`Server is running at http://silk.bot.nu on internal port ${PORT}`);
+const httpServer = http.createServer(httpApp);
+
+httpsServer.listen(process.env.HTTPS_PORT, () => {
+    console.log(`HTTPS server listening: https://${domain}:${process.env.HTTPS_PORT}`);
+});
+
+httpServer.listen(process.env.HTTP_PORT, () => {
+    console.log(`HTTP server listening: http://${domain}:${process.env.HTTP_PORT}`)
 });
