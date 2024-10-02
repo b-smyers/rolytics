218071578307f88668c7d04a5c3f50989f4bf8fd
Brendan Smyers <bs602422@ohio.edu>
2024-10-01 20:45:24 -0400

    Fixed verify route

--- src/controllers/api/v1/auth/auth.controllers.js
+++ src/controllers/api/v1/auth/auth.controllers.js
@@ -48,11 +48,8 @@ function logout(req, res) {
 }
 
 function verify(req, res) {
-    if (req.session.user) {
-        req.userId = req.session.user.id;
-        return res.status(200).json({ valid: true });
-    }
-    res.status(401).json({ valid: false });
+    const message = req.body;
+    res.status(200).json(message);
 }
 
 module.exports = {
--- src/routes/api/v1/auth.routes.js
+++ src/routes/api/v1/auth.routes.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+const { authenticate } = require('@middleware/auth.middleware');
+
 const authController = require('@controllers/api/v1/auth/auth.controllers');
 
 const router = express.Router();
@@ -6,6 +9,6 @@ const router = express.Router();
 router.post('/login', authController.login);
 router.post('/register', authController.register);
 router.post('/logout', authController.logout);
-router.post('/verify', authController.verify);
+router.post('/verify', authenticate, authController.verify);
 
 module.exports = router;
