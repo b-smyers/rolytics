3cc670bb6e67ed4e8e2956b864c99c569e8f9457
b-smyers <bs602422@ohio.edu>
2024-11-25 20:17:16 -0500

    Login page UI

--- packages/ui/src/pages/Login.jsx
+++ packages/ui/src/pages/Login.jsx
@@ -1,9 +1,55 @@
+import React, { useState } from 'react';
+import './Login.css';
+
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic (API request, validation, etc.)
+    console.log('Login attempt with:', { username, password });
+  };
+  
   return (
     <>
-        <h1>Welcome to the Login Page.</h1>
-        <p>Don&#39;t have an account?</p>
-        <a href="/register">Register Here</a>
+      <div id='background'>
+        <div id='stars'/>
+        <div id='login-box'>
+          <div id='form-box'>
+            <div id='form-header'>
+              <h1>Login</h1>
+              <img src="/icons/rolytics-android-chrome-192x192.png" alt="Rolytics Logo" />
+            </div>
+            <p>Welcome Back!</p>
+            <form id='login-form' onSubmit={handleSubmit}>
+              <input
+                type="username"
+                name="username"
+                id="username"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <a href="/404">Forgot Password?</a>
+              <button id="login-button" type="submit">
+                Login
+              </button>
+            </form>
+            <a href="/register">Don't have an account?</a>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
