d3c8a6ccb6710759181fe8cfe2d08b212de90dce
b-smyers <bs602422@ohio.edu>
2025-02-10 14:36:28 -0500

    trying lower pnpm version, pre test setup includes default env

--- packages/api/tests/jest.preSetup.js
+++ packages/api/tests/jest.preSetup.js
@@ -1,2 +1,9 @@
-require('dotenv').config();
+process.env.HTTP_PORT = 5000;
 process.env.NODE_ENV = 'test';
+process.env.SESSION_SECRET = 'session_secret';
+process.env.JWT_API_KEY_SECRET = 'jwt_api_key_secret';
+process.env.EXPERIENCE_STALE_TIME = 60000;
+process.env.PLACE_STALE_TIME = 60000;
+process.env.METRIC_MAX_AGE = 259200000;
+process.env.METRIC_CLEANUP_CRON = '0 0 * * *';
+require('dotenv').config();
