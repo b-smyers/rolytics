af8a52beec33eabfaf031b54dcec1836e68f3c2d
Brendan Smyers <bs602422@ohio.edu>
2025-04-11 22:43:59 -0400

    spaces 4 -> 2

--- packages/ui/src/layouts/AppLayout.jsx
+++ packages/ui/src/layouts/AppLayout.jsx
@@ -1,69 +1,70 @@
+import { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import Protected from '../components/Protected';
 import './AppLayout.css';
 
 const SidbarButton = ({ icon, name, uri, isThemed = false }) => {
-    const navigate = useNavigate();
-    const navigateURI = () => { navigate(uri); };
-  
-    return (
-        <button
-            className="sidebar-nav-item"
-            onClick={navigateURI}
-            onKeyUp={(input) => {
-                if (input.key === 'Enter') navigateURI();
-            }}
-            tabIndex={0} // Makes the div focusable for accessibility
-            aria-label={name}>
-            {<img src={icon} alt={name} className={`sidebar-icon ${isThemed ? 'themed' : ''}`} />}
-            {name && <span className="sidebar-name">{name}</span>}
-        </button>
-    )
+  const navigate = useNavigate();
+  const navigateURI = () => { navigate(uri); };
+
+  return (
+    <button
+      className="sidebar-nav-item"
+      onClick={navigateURI}
+      onKeyUp={(input) => {
+        if (input.key === 'Enter') navigateURI();
+      }}
+      tabIndex={0} // Makes the div focusable for accessibility
+      aria-label={name}>
+      {<img src={icon} alt={name} className={`sidebar-icon ${isThemed ? 'themed' : ''}`} />}
+      {name && <span className="sidebar-name">{name}</span>}
+    </button>
+  )
 };
 
 function AppLayout() {
-    const date = new Date();
-    let year = date.getFullYear();
+  const date = new Date();
+  let year = date.getFullYear();
 
-    return (
-        <div className="layout">
-            <aside className={"sidebar"}>
-                <div className="sidebar-top">
-                    <SidbarButton icon={"/logos/rolytics.svg"} name={"Rolytics"} uri={"/"}/>
-                    <Protected>
-                        <SidbarButton 
-                            icon={"/icons/home.svg"} 
-                            name={"Dashboard"} 
-                            uri={"/dashboard"}
-                            isThemed={true}
-                        />
-                    </Protected>
-                </div>
-                <div className="sidebar-bottom">
-                    <SidbarButton
-                        icon={"/icons/missing.svg"}
-                        name={"Register"}
-                        uri={"/register"}
-                        isThemed={true}
-                    />
-                    <SidbarButton
-                        icon={"/icons/login.svg"}
-                        name={"Login"}
-                        uri={"/login"}
-                        isThemed={true}
-                    />
-                </div>
-            </aside>
-            <div className="main-content">
-                <div className='fill-page'>
-                    <Outlet />
-                </div>
-                <footer>
-                    <p>&copy;Rolytics {year} — Rolytics is not affiliated with or endorsed by Roblox Corporation — All trademarks and copyrights are the property of their respective owners.</p>
-                </footer>
-            </div>
-        </div>
-    );
+  return (
+    <div className="layout">
+      <aside className={"sidebar"}>
+        <div className="sidebar-top">
+          <SidbarButton icon={"/logos/rolytics.svg"} name={"Rolytics"} uri={"/"}/>
+          <Protected>
+            <SidbarButton 
+              icon={"/icons/home.svg"} 
+              name={"Dashboard"} 
+              uri={"/dashboard"}
+              isThemed={true}
+            />
+          </Protected>
+      </div>
+      <div className="sidebar-bottom">
+        <SidbarButton
+          icon={"/icons/missing.svg"}
+          name={"Register"}
+          uri={"/register"}
+          isThemed={true}
+        />
+        <SidbarButton
+          icon={"/icons/login.svg"}
+          name={"Login"}
+          uri={"/login"}
+          isThemed={true}
+        />
+      </div>
+    </aside>
+    <div className="main-content">
+      <div className='fill-page'>
+        <Outlet />
+      </div>
+      <footer>
+        <p>&copy;Rolytics {year} — Rolytics is not affiliated with or endorsed by Roblox Corporation — All trademarks and copyrights are the property of their respective owners.</p>
+      </footer>
+      </div>
+    </div>
+  );
 }
 
 export default AppLayout;
