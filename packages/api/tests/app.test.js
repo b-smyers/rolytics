bc25ce54936ff605487a29806e75c7d62e499409
Brendan Smyers <bs602422@ohio.edu>
2025-07-21 23:22:33 -0400

    Added more endpoint tests

--- packages/api/tests/app.test.js
+++ packages/api/tests/app.test.js
@@ -2,48 +2,31 @@ const request = require("supertest");
 const http = require("http");
 const app = require("@api/app").default;
 
+const apiBaseUrl = "/api/v1";
+const authBaseUrl = apiBaseUrl + "/auth";
+const experiencesBaseUrl = apiBaseUrl + "/experiences";
+const placesBaseUrl = apiBaseUrl + "/places";
+const robloxBaseUrl = apiBaseUrl + "/roblox";
+const usersBaseUrl = apiBaseUrl + "/users";
+
 async function getFreshApiKey() {
   const username = `testuser_${Date.now()}`;
   const email = `testuser_${Date.now()}@example.com`;
   const password = "password";
   const agent = request.agent(app);
 
-  await agent.post("/api/v1/auth/register").send({
+  await agent.post(authBaseUrl + "/register").send({
     username,
     email,
     password,
   });
-  await agent.post("/api/v1/auth/login").send({ username, password });
+  await agent.post(authBaseUrl + "/login").send({ username, password });
 
   const profile = await agent.get("/api/v1/users/profile");
   return profile.body.data.profile.api_key;
 }
 
 describe("Integration Tests", () => {
-  const apiBaseUrl = "/api/v1";
-  const authBaseUrl = apiBaseUrl + "/auth";
-  const usersBaseUrl = apiBaseUrl + "/users";
-  const experiencesBaseUrl = apiBaseUrl + "/experiences";
-
-  let apiKey;
-
-  beforeAll(async () => {
-    // Create a test user and fetch its API key
-    const agent = request.agent(app);
-    await agent.post(authBaseUrl + "/register").send({
-      username: "user1",
-      email: "user1@example.com",
-      password: "password",
-    });
-
-    await agent
-      .post(authBaseUrl + "/login")
-      .send({ username: "user1", password: "password" });
-
-    const profile = await agent.get(usersBaseUrl + "/profile");
-    apiKey = profile.body.data.profile.api_key;
-  });
-
   describe("Smoke Test", () => {
     let httpServer;
     beforeAll((done) => {
@@ -67,7 +50,7 @@ describe("Integration Tests", () => {
     });
   });
 
-  describe("Auth", () => {
+  describe("/auth", () => {
     describe("POST /register", () => {
       it("should return 200 and register successfully", async () => {
         await request(app)
@@ -188,10 +171,15 @@ describe("Integration Tests", () => {
     });
 
     describe("POST /verify", () => {
+      let testKey;
+      beforeAll(async () => {
+        testKey = await getFreshApiKey();
+      });
+
       it("should return 200 and verify successfully with message", async () => {
         await request(app)
           .post(authBaseUrl + "/verify")
-          .set("Authorization", `Bearer ${await getFreshApiKey()}`)
+          .set("Authorization", `Bearer ${testKey}`)
           .send({ message: "hello, world" })
           .expect(200)
           .expect((res) => {
@@ -208,7 +196,7 @@ describe("Integration Tests", () => {
       it("should return 200 and verify successfully without message", async () => {
         await request(app)
           .post(authBaseUrl + "/verify")
-          .set("Authorization", `Bearer ${await getFreshApiKey()}`)
+          .set("Authorization", `Bearer ${testKey}`)
           .expect(200)
           .expect((res) => {
             expect(res.body).toEqual({
@@ -223,12 +211,26 @@ describe("Integration Tests", () => {
     });
   });
 
-  describe("Experiences", () => {
+  describe("/experiences", () => {
     describe("GET /", () => {
+      let testKey;
+      beforeAll(async () => {
+        testKey = await getFreshApiKey();
+        await request(app)
+          .post(experiencesBaseUrl + "/connect")
+          .set("Authorization", `Bearer ${testKey}`)
+          .send({
+            roblox_experience_id: 5195557959,
+            page_link: "https://www.roblox.com/games/5195557959",
+            name: "Dungeon Odyssey",
+          })
+          .expect(200);
+      });
+
       it("should return 200 and list all experiences", async () => {
         await request(app)
           .get(experiencesBaseUrl)
-          .set("Authorization", `Bearer ${await getFreshApiKey()}`)
+          .set("Authorization", `Bearer ${testKey}`)
           .send()
           .expect(200)
           .expect((res) => {
@@ -237,7 +239,7 @@ describe("Integration Tests", () => {
               status: "success",
               data: {
                 message: "Experiences successfully retrieved",
-                experiences: [],
+                experiences: expect.any(Array),
               },
             });
           });
@@ -245,15 +247,15 @@ describe("Integration Tests", () => {
     });
 
     describe("POST /connect", () => {
-      let connectApiKey;
+      let testKey;
       beforeAll(async () => {
-        connectApiKey = await getFreshApiKey();
+        testKey = await getFreshApiKey();
       });
 
       it("should return 200 and connect to an experience", async () => {
         await request(app)
           .post(experiencesBaseUrl + "/connect")
-          .set("Authorization", `Bearer ${connectApiKey}`)
+          .set("Authorization", `Bearer ${testKey}`)
           .send({
             roblox_experience_id: 5195557959,
             page_link: "https://www.roblox.com/games/5195557959",
@@ -274,7 +276,7 @@ describe("Integration Tests", () => {
       it("should return 400 if user already connected the experience", async () => {
         await request(app)
           .post(experiencesBaseUrl + "/connect")
-          .set("Authorization", `Bearer ${connectApiKey}`)
+          .set("Authorization", `Bearer ${testKey}`)
           .send({
             roblox_experience_id: 5195557959,
             page_link: "https://www.roblox.com/games/5195557959",
@@ -331,43 +333,409 @@ describe("Integration Tests", () => {
     });
 
     describe("GET /performance", () => {
-    //   let performanceApiKey;
+      let testKey;
+      let experienceId;
+      beforeAll(async () => {
+        testKey = await getFreshApiKey();
+        await request(app)
+          .post(experiencesBaseUrl + "/connect")
+          .set("Authorization", `Bearer ${testKey}`)
+          .send({
+            roblox_experience_id: 5195557959,
+            page_link: "https://www.roblox.com/games/5195557959",
+            name: "Dungeon Odyssey",
+          })
+          .expect(200);
+
+        const result = await request(app)
+          .get(experiencesBaseUrl)
+          .set("Authorization", `Bearer ${testKey}`)
+          .expect(200);
+
+        experienceId = result.body.data.experiences[0].experience_id;
+      });
+
+      it("should return 200 and performance data", async () => {
+        await request(app)
+          .get(
+            experiencesBaseUrl + `/performance?experience_id=${experienceId}`
+          )
+          .set("Authorization", `Bearer ${testKey}`)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body).toEqual({
+              code: 200,
+              status: "success",
+              data: {
+                message: "Performance data successfully retrieved",
+                keys: expect.any(Array),
+                data: expect.any(Array),
+              },
+            });
+          });
+      });
+    });
+
+    describe("GET /players", () => {
+      let testKey;
+      let experienceId;
+      beforeAll(async () => {
+        testKey = await getFreshApiKey();
+        await request(app)
+          .post(experiencesBaseUrl + "/connect")
+          .set("Authorization", `Bearer ${testKey}`)
+          .send({
+            roblox_experience_id: 5195557959,
+            page_link: "https://www.roblox.com/games/5195557959",
+            name: "Dungeon Odyssey",
+          })
+          .expect(200);
+
+        const result = await request(app)
+          .get(experiencesBaseUrl)
+          .set("Authorization", `Bearer ${testKey}`)
+          .expect(200);
+
+        experienceId = result.body.data.experiences[0].experience_id;
+      });
+
+      it("should return 200 and players data", async () => {
+        await request(app)
+          .get(experiencesBaseUrl + `/players?experience_id=${experienceId}`)
+          .set("Authorization", `Bearer ${testKey}`)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body).toEqual({
+              code: 200,
+              status: "success",
+              data: {
+                message: "Players data successfully retrieved",
+                keys: expect.any(Array),
+                data: expect.any(Array),
+              },
+            });
+          });
+      });
+    });
+
+    describe("GET /purchases", () => {
+      let testKey;
+      let experienceId;
+      beforeAll(async () => {
+        testKey = await getFreshApiKey();
+        await request(app)
+          .post(experiencesBaseUrl + "/connect")
+          .set("Authorization", `Bearer ${testKey}`)
+          .send({
+            roblox_experience_id: 5195557959,
+            page_link: "https://www.roblox.com/games/5195557959",
+            name: "Dungeon Odyssey",
+          })
+          .expect(200);
+
+        const result = await request(app)
+          .get(experiencesBaseUrl)
+          .set("Authorization", `Bearer ${testKey}`)
+          .expect(200);
+
+        experienceId = result.body.data.experiences[0].experience_id;
+      });
+
+      it("should return 200 and purchases data", async () => {
+        await request(app)
+          .get(experiencesBaseUrl + `/purchases?experience_id=${experienceId}`)
+          .set("Authorization", `Bearer ${testKey}`)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body).toEqual({
+              code: 200,
+              status: "success",
+              data: {
+                message: "Purchases data successfully retrieved",
+                keys: expect.any(Array),
+                data: expect.any(Array),
+              },
+            });
+          });
+      });
+    });
+
+    describe("GET /social", () => {
+      let testKey;
+      let experienceId;
+      beforeAll(async () => {
+        testKey = await getFreshApiKey();
+        await request(app)
+          .post(experiencesBaseUrl + "/connect")
+          .set("Authorization", `Bearer ${testKey}`)
+          .send({
+            roblox_experience_id: 5195557959,
+            page_link: "https://www.roblox.com/games/5195557959",
+            name: "Dungeon Odyssey",
+          })
+          .expect(200);
+
+        const result = await request(app)
+          .get(experiencesBaseUrl)
+          .set("Authorization", `Bearer ${testKey}`)
+          .expect(200);
+
+        experienceId = result.body.data.experiences[0].experience_id;
+      });
+
+      it("should return 200 and social data", async () => {
+        await request(app)
+          .get(experiencesBaseUrl + `/social?experience_id=${experienceId}`)
+          .set("Authorization", `Bearer ${testKey}`)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body).toEqual({
+              code: 200,
+              status: "success",
+              data: {
+                message: "Social data successfully retrieved",
+                keys: expect.any(Array),
+                data: expect.any(Array),
+              },
+            });
+          });
+      });
+    });
+  });
+
+  describe("/places", () => {
+    let testKey;
+    let experienceId;
+    beforeAll(async () => {
+      testKey = await getFreshApiKey();
+      await request(app)
+        .post(experiencesBaseUrl + "/connect")
+        .set("Authorization", `Bearer ${testKey}`)
+        .send({
+          roblox_experience_id: 5195557959,
+          page_link: "https://www.roblox.com/games/5195557959",
+          name: "Dungeon Odyssey",
+        })
+        .expect(200);
+
+      const result = await request(app)
+        .get(experiencesBaseUrl)
+        .set("Authorization", `Bearer ${testKey}`)
+        .expect(200);
+
+      experienceId = result.body.data.experiences[0].experience_id;
+    });
+
+    describe("GET /", () => {
+      it("should return 200 and list all places", async () => {
+        await request(app)
+          .get(placesBaseUrl + `?experience_id=${experienceId}`)
+          .set("Authorization", `Bearer ${testKey}`)
+          .send()
+          .expect(200)
+          .expect((res) => {
+            expect(res.body).toEqual({
+              code: 200,
+              status: "success",
+              data: {
+                message: "Places successfully retrieved",
+                places: expect.any(Array),
+              },
+            });
+          });
+      });
+    });
+
+    describe("GET /analytics", () => {
+      it("should return 501 for not implemented", async () => {
+        await request(app)
+          .get(placesBaseUrl + "/analytics")
+          .set("Authorization", `Bearer ${await getFreshApiKey()}`)
+          .expect(501)
+          .expect((res) => {
+            expect(res.body).toEqual({
+              code: 501,
+              status: "error",
+              data: {
+                message: "Not Implemented",
+              },
+            });
+          });
+      });
+    });
+
+    describe("GET /performance", () => {
+      let placeId;
+      beforeAll(async () => {
+        const result = await request(app)
+          .get(placesBaseUrl + `?experience_id=${experienceId}`)
+          .set("Authorization", `Bearer ${testKey}`);
+
+        placeId = result.body.data.places[0].place_id;
+      });
+
+      it("should return 200 and performance data", async () => {
+        await request(app)
+          .get(placesBaseUrl + `/performance?place_id=${placeId}`)
+          .set("Authorization", `Bearer ${testKey}`)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body).toEqual({
+              code: 200,
+              status: "success",
+              data: {
+                message: "Performance data successfully retrieved",
+                keys: expect.any(Array),
+                data: expect.any(Array),
+              },
+            });
+          });
+      });
+    });
+
+    describe("GET /players", () => {
+      let placeId;
+      beforeAll(async () => {
+        const result = await request(app)
+          .get(placesBaseUrl + `?experience_id=${experienceId}`)
+          .set("Authorization", `Bearer ${testKey}`);
+
+        placeId = result.body.data.places[0].place_id;
+      });
+
+      it("should return 200 and players data", async () => {
+        await request(app)
+          .get(placesBaseUrl + `/players?place_id=${placeId}`)
+          .set("Authorization", `Bearer ${testKey}`)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body).toEqual({
+              code: 200,
+              status: "success",
+              data: {
+                message: "Players data successfully retrieved",
+                keys: expect.any(Array),
+                data: expect.any(Array),
+              },
+            });
+          });
+      });
+    });
+
+    describe("GET /purchases", () => {
+      let placeId;
+      beforeAll(async () => {
+        const result = await request(app)
+          .get(placesBaseUrl + `?experience_id=${experienceId}`)
+          .set("Authorization", `Bearer ${testKey}`);
+
+        placeId = result.body.data.places[0].place_id;
+      });
+
+      it("should return 200 and purchases data", async () => {
+        await request(app)
+          .get(placesBaseUrl + `/purchases?place_id=${placeId}`)
+          .set("Authorization", `Bearer ${testKey}`)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body).toEqual({
+              code: 200,
+              status: "success",
+              data: {
+                message: "Purchases data successfully retrieved",
+                keys: expect.any(Array),
+                data: expect.any(Array),
+              },
+            });
+          });
+      });
+    });
+
+    describe("GET /social", () => {
+      let placeId;
+      beforeAll(async () => {
+        const result = await request(app)
+          .get(placesBaseUrl + `?experience_id=${experienceId}`)
+          .set("Authorization", `Bearer ${testKey}`);
+
+        placeId = result.body.data.places[0].place_id;
+      });
+
+      it("should return 200 and social data", async () => {
+        await request(app)
+          .get(placesBaseUrl + `/social?place_id=${placeId}`)
+          .set("Authorization", `Bearer ${testKey}`)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body).toEqual({
+              code: 200,
+              status: "success",
+              data: {
+                message: "Social data successfully retrieved",
+                keys: expect.any(Array),
+                data: expect.any(Array),
+              },
+            });
+          });
+      });
+    });
+  });
+
+  describe("/roblox", () => {
+    // describe("POST /place-details", () => {
+    //   let testKey;
+    //   let experienceId;
+    //   let placeId;
     //   beforeAll(async () => {
-    //     performanceApiKey = await getFreshApiKey();
+    //     testKey = await getFreshApiKey();
     //     await request(app)
     //       .post(experiencesBaseUrl + "/connect")
-    //       .set("Authorization", `Bearer ${performanceApiKey}`)
+    //       .set("Authorization", `Bearer ${testKey}`)
     //       .send({
     //         roblox_experience_id: 5195557959,
     //         page_link: "https://www.roblox.com/games/5195557959",
     //         name: "Dungeon Odyssey",
     //       })
     //       .expect(200);
+
+    //     const experiencesResult = await request(app)
+    //       .get(experiencesBaseUrl)
+    //       .set("Authorization", `Bearer ${testKey}`)
+    //       .expect(200);
+
+    //     experienceId = experiencesResult.body.data.experiences[0].experience_id;
+
+    //     const placesResult = await request(app)
+    //       .get(placesBaseUrl + `?experience_id=${experienceId}`)
+    //       .set("Authorization", `Bearer ${testKey}`)
+    //       .expect(200);
+
+    //     placeId = placesResult.body.data.places[0].place_id;
     //   });
 
-    //   it("should return 200 and performance data", async () => {
+    //   it("should return 200 and place details", async () => {
     //     await request(app)
-    //       .get(experiencesBaseUrl + "/performance?experience_id=5195557959")
-    //       .set("Authorization", `Bearer ${performanceApiKey}`)
+    //       .post(robloxBaseUrl + "/place-details")
+    //       .set("Authorization", `Bearer ${testKey}`)
+    //       .send({ place_id: placeId })
     //       .expect(200)
     //       .expect((res) => {
     //         expect(res.body).toEqual({
     //           code: 200,
     //           status: "success",
     //           data: {
-    //             message: "Performance data successfully retrieved",
-    //             keys: expect.any(Array),
-    //             data: expect.any(Array),
+    //             message: "Place details successfully retrieved",
+    //             placeDetails: expect.objectContaining({
+    //               place_id: expect.any(Number),
+    //               name: expect.any(String),
+    //               description: expect.any(String),
+    //               experienceId: expect.any(Number),
+    //               thumbnails: expect.any(Array),
+    //             }),
     //           },
     //         });
     //       });
     //   });
-    });
-
-    describe("GET /players", () => {});
-
-    describe("GET /purchases", () => {});
-
-    describe("GET /social", () => {});
+    // });
   });
 });
